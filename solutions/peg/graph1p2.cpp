8158150305a51d2aeabceb9d34551604892218c8
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2018-03-05 17:21:50 -0500

    Create graph1p2.cpp

---
+++ solutions/peg/graph1p2.cpp
@@ -0,0 +1,45 @@
+/*
+ Solution to Shortest Path by Ava Pun
+ Key concepts: graph theory, Dijkstra's
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+bool adj[105][105];
+int dist[105];
+bool vis[105];
+
+int main() {
+    
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    
+    dist[0] = 0;
+    vis[0] = true;
+    queue<int> q;
+    q.push(0);
+    
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (int i = 0; i < N; i++) {
+            if (adj[x][i] && !vis[i]) {
+                vis[i] = true;
+                dist[i] = dist[x] + 1;
+                q.push(i);
+            }
+        }
+    }
+    
+    printf("%d", dist[N - 1]);
+    
+    return 0;
+    
+}
