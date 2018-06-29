9e48f699a3ee329da2412b5738cf1f4fa8ced1d8
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2018-06-28 22:04:06 -0400

    Update cco18p5.cpp

--- solutions/ccc/cco18p5.cpp
+++ solutions/ccc/cco18p5.cpp
@@ -22,36 +22,50 @@ const int MOD = 1e9 + 7;
 const int MAX = 1e6 + 5;
 
 int N, K, Q;
-pii maxTree[2 * MAX];
-int locTree[2 * MAX];
+int arr[MAX];
+int maxTree[2 * MAX], locTree[2 * MAX];
+deque<pii> q;
+int maxes[2 * MAX];
 
-void update(int pos, int x, pii tree[]) {
-    pos += N - 1;
-    tree[pos].f = x;
-    for (int i = pos >> 1; i > 0; i >>= 1) {
-        tree[i] = max(tree[i << 1], tree[i << 1 | 1]);
+inline bool comp(int a, int b) {
+    if (a == 0) {
+        return 1;
+    } else if (b == 0) {
+        return 0;
+    } else if (arr[a] == arr[b]) {
+        return a < b;
+    } else {
+        return arr[a] < arr[b];
     }
 }
 
-void update(int pos, int x, int tree[]) {
+inline void updMax(int pos, int x) {
+    arr[pos] = x;
     pos += N - 1;
-    tree[pos] = x;
     for (int i = pos >> 1; i > 0; i >>= 1) {
-        tree[i] = max(tree[i << 1], tree[i << 1 | 1]);
+        maxTree[i] = max(maxTree[i << 1], maxTree[i << 1 | 1], comp);
     }
 }
 
-pii query(int l, int r, pii tree[]) {
+inline void updLoc(int pos, int x) {
+    pos += N - 1;
+    locTree[pos] = x;
+    for (int i = pos >> 1; i > 0; i >>= 1) {
+        locTree[i] = max(locTree[i << 1], locTree[i << 1 | 1]);
+    }
+}
+
+inline int query(int l, int r) {
     l = max(l, 1), r = min(r, N);
     l += N - 1, r += N - 1;
-    pii ans = {-1, -1};
+    int ans = 0;
     while (l <= r) {
         if (l & 1) {
-            ans = max(ans, tree[l]);
+            ans = max(ans, maxTree[l], comp);
             l++;
         }
         if (!(r & 1)) {
-            ans = max(ans, tree[r]);
+            ans = max(ans, maxTree[r], comp);
             r--;
         }
         l >>= 1, r >>= 1;
@@ -59,50 +73,70 @@ pii query(int l, int r, pii tree[]) {
     return ans;
 }
 
-void checkLoc(int i) {
-    int val = maxTree[i + N - 1].f;
-    int maxL = query(i - K + 1, i - 1, maxTree).f, maxR = query(i + 1, i + K - 1, maxTree).f;
-    if (val >= maxL && val > maxR) {
-        locTree[i + N - 1] = val + max(maxL, maxR);
-    } else {
-        locTree[i + N - 1] = -1;
-    }
+inline pii maxLR(int i) {
+    return {query(i - K + 1, i - 1), query(i + 1, i + K - 1)};
 }
 
-void checkLoc1(int i) {
-    checkLoc(i), update(i, locTree[i + N - 1], locTree);
+inline void checkLoc(int i) {
+    pii lr = maxLR(i);
+    if (arr[i] >= arr[lr.f] && arr[i] > arr[lr.s]) {
+        updLoc(i, arr[i] + max(arr[lr.f], arr[lr.s]));
+    }
 }
 
 int main() {
     
     scanf("%d%d%d", &N, &K, &Q);
+    arr[0] = -1;
     for (int i = 1; i <= N; i++) {
-        scanf("%d", &maxTree[i + N - 1].f);
-    }
-    for (int i = 1; i <= N; i++) {
-        maxTree[i + N - 1].s = i;
+        scanf("%d", &arr[i]);
+        maxTree[i + N - 1] = i;
     }
     for (int i = N - 1; i > 0; i--) {
-        maxTree[i] = max(maxTree[i << 1], maxTree[i << 1 | 1]);
+        maxTree[i] = max(maxTree[i << 1], maxTree[i << 1 | 1], comp);
+    }
+    for (int i = 1; i <= N + K - 1; i++) {
+        if (!q.empty() && q.front().s == i - K + 1) {
+            q.pop_front();
+        }
+        if (i <= N) {
+            while (!q.empty() && q.back().f <= arr[i]) {
+                q.pop_back();
+            }
+            q.push_back({arr[i], i});
+        }
+        maxes[i] = q.front().s;
     }
     for (int i = 1; i <= N; i++) {
-        checkLoc(i);
+        pii lr = {i == 1 ? 0 : maxes[i - 1], i == N ? 0 : maxes[i + K - 1]};
+        if (arr[i] >= arr[lr.f] && arr[i] > arr[lr.s]) {
+            locTree[i + N - 1] = arr[i] + max(arr[lr.f], arr[lr.s]);
+        }
     }
     for (int i = N - 1; i > 0; i--) {
         locTree[i] = max(locTree[i << 1], locTree[i << 1 | 1]);
     }
-    
+
     int ans = locTree[1];
     printf("%d\n", ans);
     
     int i, x;
     while (Q--) {
         scanf("%d%d", &i, &x);
-        //i ^= ans, x ^= ans;
-        update(i, x, maxTree);
-        checkLoc1(i);
-        int maxL = query(i - K + 1, i - 1, maxTree).s, maxR = query(i + 1, i + K - 1, maxTree).s;
-        checkLoc1(maxL), checkLoc1(maxR);
+        i ^= ans, x ^= ans;
+        updMax(i, x);
+        pii lr = maxLR(i);
+        if (x >= arr[lr.f] && x > arr[lr.s]) {
+            updLoc(i, x + max(arr[lr.f], arr[lr.s]));
+        } else {
+            updLoc(i, 0);
+            if (lr.f != 0) {
+                checkLoc(lr.f);
+            }
+            if (lr.s != 0) {
+                checkLoc(lr.s);
+            }
+        }
         ans = locTree[1];
         printf("%d\n", ans);
     }
