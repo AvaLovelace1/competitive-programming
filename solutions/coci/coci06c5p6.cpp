78c406e2c9dbf6e48831886f0b44e7b1f1a39384
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2019-03-14 14:56:24 -0400

    Update coci06c5p6.cpp

--- solutions/coci/coci06c5p6.cpp
+++ solutions/coci/coci06c5p6.cpp
@@ -1,93 +1,84 @@
 /*
  Solution to COCI '06 Contest 5 #6 Dvaput by Ava Pun
- Key concepts: suffix array
+ Key concepts: hashing, binary search
  */
  
 #include <bits/stdc++.h>
 
 using namespace std;
 
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
 typedef long long int ll;
 typedef pair<int, int> pii;
 typedef pair<int, pii> piii;
-const int INF = 0x3F3F3F3F;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
 const int MOD = 1e9 + 7;
-const int MAX = 200005;
+const int MAX = 2e5 + 5;
+const int BASE = 29;
 
 int N;
-string S;
-piii suff[MAX];
-int idx[MAX], lcp[MAX];
+char S[MAX];
+ll power[MAX], hsh[MAX];
+unordered_map<ll, int> ss;
 
-bool comp(piii a, piii b) {
-    return a.second < b.second;
+inline ll getHsh(int l, int r) {
+    return (hsh[r] - hsh[l - 1] * power[r - l + 1] % MOD + MOD) % MOD;
+}
+
+inline bool verify(int a, int b, int n) {
+    for (int i = 0; i < n; i++) {
+        if (S[a + i] != S[b + i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+inline bool check(int n) {
+    ss.clear();
+    for (int i = 1; i + n - 1 <= N; i++) {
+        ll h = getHsh(i, i + n - 1);
+        if (ss.find(h) != ss.end() && verify(i, ss[h], n)) {
+            return 1;
+        } else {
+            ss[h] = i;
+        }
+    }
+    return 0;
 }
 
 int main() {
     
-    cin.sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> N;
-    cin >> S;
-    
-    for (int i = 0; i < N; i++) {
-        suff[i] = {i, {S[i], i + 1 < N ? S[i + 1] : -1}};
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        scanf(" %c", &S[i]);
     }
     
-    sort(suff, suff + N, comp);
-    
-    for (int k = 4; k < 2 * N; k *= 2) {
-        
-        int rank = 0, prev = suff[0].second.first;
-        suff[0].second.first = rank;
-        idx[suff[0].first] = 0;
-        
-        for (int i = 1; i < N; i++) {
-            if (!(suff[i].second.first == prev && suff[i].second.second == suff[i - 1].second.second)) {
-                rank++;
-            }
-            prev = suff[i].second.first;
-            suff[i].second.first = rank;
-            idx[suff[i].first] = i;
-        }
-        
-        for (int i = 0; i < N; i++) {
-            int next = suff[i].first + k / 2;
-            suff[i].second.second = next < N ? suff[idx[next]].second.first : -1;
-        }
-        
-        sort(suff, suff + N, comp);
-        
+    power[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        power[i] = power[i - 1] * BASE % MOD;
+    }
+    for (int i = 1; i <= N; i++) {
+        hsh[i] = (hsh[i - 1] * BASE % MOD + (S[i] - 'a' + 1)) % MOD;
     }
     
-    for (int i = 0; i < N; i++) {
-        idx[suff[i].first] = i;
-    }
-    
-    int len = 0;
-    
-    for (int i = 0; i < N; i++) {
-        if (idx[i] != N - 1) {
-            int j = suff[idx[i] + 1].first;
-            while (i + len < N && j + len < N && S[i + len] == S[j + len]) {
-                len++;
-            }
+    int lo = 1, hi = N + 1, mid;
+    while (lo < hi) {
+        mid = (lo + hi) / 2;
+        if (!check(mid)) {
+            hi = mid;
         } else {
-            len = 0;
-        }
-        lcp[idx[i]] = len;
-        if (len > 0) {
-            len--;
+            lo = mid + 1;
         }
     }
-    
-    int maxN = 0;
-    for (int i = 0; i < N; i++) {
-        maxN = max(maxN, lcp[i]);
-    }
-    
-    cout << maxN;
+    printf("%d\n", lo - 1);
     
     return 0;
 }
