60e08401d90edc1e92946162c2c54257ff50bda8
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2019-07-24 16:53:28 -0400

    Create acc5p1.cpp

---
+++ solutions/dmoj/acc5p1.cpp
@@ -0,0 +1,33 @@
+/*
+ Solution to Another Contest 5 Problem 1 - Goat Fence by Ava Pun
+ Key concepts: implementation, simple math
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+int N;
+
+int main() {
+    
+    scanf("%d", &N);
+    printf("%d\n", 2 * N + 2);
+    
+    return 0;
+}
