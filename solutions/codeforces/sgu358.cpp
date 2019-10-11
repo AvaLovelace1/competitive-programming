479524e402804433a2141fd507469a14ccd18134
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2019-10-11 13:10:14 -0400

    Create sgu358.cpp

---
+++ solutions/codeforces/sgu358.cpp
@@ -0,0 +1,41 @@
+/*
+ Solution to Median of Medians by Ava Pun
+ Key concepts: implementation
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+const int INF = 0x3F3F3F3F;
+const ll INFL = 0x3F3F3F3F3F3F3F3FLL;
+const int MOD = 1e9 + 7;
+const int MAX = 2e5 + 5;
+const double EPS = 1e-9;
+
+int arr[3][3], m[3];
+
+int main() {
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+        sort(arr[i], arr[i] + 3);
+        m[i] = arr[i][1];
+    }
+    sort(m, m + 3);
+    printf("%d\n", m[1]);
+
+    return 0;
+}
