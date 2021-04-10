9263623d0c7d9c9409b73d29acfe7e7e257dcaf9
AvaLovelace1 <flatthefish@gmail.com>
2021-04-10 08:31:12 -0400

    Add CF 545D

---
+++ solutions/codeforces/545D.cpp
@@ -0,0 +1,60 @@
+/*
+ * Solution to Queue by Ava Pun
+ * Key concepts: greedy algorithms, sorting
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+#define REP(i, a, b) for (int i = a; i <= (b); ++i)
+#define REV(i, a, b) for (int i = a; i >= (b); --i)
+#define TRAV(a, x) for (auto &a : x)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (int) (x).size()
+
+using ll = long long;
+using pii = pair<int, int>;
+using piii = pair<pii, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+const int INF = 0x3F3F3F3F;
+const ll INFL = 0x3F3F3F3F3F3F3F3FLL;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int MAX = 1e5 + 5;
+
+int N;
+int arr[MAX];
+
+int calc() {
+    ll tot = 0;
+    int ret = 0;
+    REP(i, 1, N) {
+        if (tot <= arr[i]) {
+            tot += arr[i];
+            ++ret;
+        }
+    }
+    return ret;
+}
+
+int main() {
+
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin.exceptions(cin.failbit);
+
+    cin >> N;
+    REP(i, 1, N) {
+        cin >> arr[i];
+    }
+    sort(arr + 1, arr + N + 1);
+    cout << calc() << '\n';
+
+}
+
